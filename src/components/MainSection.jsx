fa0a717ef810ad77dfc854a3e633e8434195338b
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-05-25 22:29:14 +0530

    Added MainSection Component

---
+++ src/components/MainSection.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./MainSection.css";
+import Input from "./Input";
+
+const MainSection = () => {
+  return (
+    <div className="main-section">
+      <div className="expected-phrase">
+        <p>ac ba ac ba ac ba</p>
+      </div>
+      <Input />
+    </div>
+  );
+};
+
+export default MainSection;
