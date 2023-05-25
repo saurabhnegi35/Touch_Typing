1d83fa5a4f13e8b87fe66051b23a7192f094b586
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-05-25 22:30:26 +0530

    Added Input Component

---
+++ src/components/Input.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Input.css";
+
+const Input = () => {
+  return (
+    <>
+      <input
+        type="text"
+        className="input-bar"
+        placeholder="Type the Upper Text Here"
+      />
+    </>
+  );
+};
+
+export default Input;
