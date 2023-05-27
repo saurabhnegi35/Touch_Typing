638c0b61792de2193a7bd6a381b93d81d6e65991
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-05-27 22:18:21 +0530

    Added Accuracy, show Error Count and Total Characters Typed Functionality

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,18 +8,21 @@ const Footer = () => {
         <h3 className="title-section">Touch Typing</h3>
       </div>
       <div className="footer-info">
-        <a href="https://github.com">
+        <a
+          href="https://github.com/saurabhnegi35/Touch_Typing#readme"
+          target="_blank"
+        >
           <i className="fa-solid fa-circle-question"></i> Rules to Play
         </a>
-        <a href="https://github.com">
+        <a href="https://github.com/saurabhnegi35" target="_blank">
           <i className="fa-solid fa-wrench"></i> Suggest Improvements
         </a>
-        <a href="https://github.com">
+        <a href="https://github.com/saurabhnegi35/Touch_Typing" target="_blank">
           <i className="fa-brands fa-github"></i> Github Repository
         </a>
       </div>
       <div className="footer-info">
-        <a href="https://github.com">
+        <a href="https://react.dev/" target="_blank">
           Powered By <i className="fa-brands fa-react"></i>
         </a>
       </div>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -14,28 +14,29 @@ const Input = ({
   setTimerPaused,
   setMatched,
 }) => {
-  // console.log(displayWord);
   const [word, setWord] = useState("");
   const [isTrue, setIsTrue] = useState("");
   const [isCompleted, setIsCompleted] = useState(false);
   const [play, setPlay] = useState("false");
-  // const [isTimerPaused, setIsTimerPaused] = useState(false);
+  const [errorCount, setErrorCount] = useState(0);
+  const [totalWordCount, setTotalWordCount] = useState(0);
+  const [accuracy, setAccuracy] = useState(100);
 
-  // console.log(play);
   useEffect(() => {
     setPlay("true");
   }, [displayWord]);
 
-  // console.log(isTimerPaused);
   let inputBackgroundColor;
 
   const handleKeyDown = () => {
     if (!isKeyPressed) {
       setKeyPressed(true);
       setTimerPaused(false);
+    } else {
+      setTotalWordCount((prevCount) => prevCount + 1);
     }
   };
-
+  console.log("setTotalWordCount", totalWordCount);
   function click() {
     if (play === "true") {
       new Audio(Click).play();
@@ -44,6 +45,7 @@ const Input = ({
 
   function error() {
     new Audio(Error).play();
+    setErrorCount((prevCount) => prevCount + 1);
   }
 
   function yay() {
@@ -51,20 +53,6 @@ const Input = ({
       new Audio(Yay).play();
     }
   }
-
-  // console.log("Word Length", word.length);
-  // console.log("Word Length", word[word.length - 1]);
-  // console.log("Display Word length ", props.displayWord[word.length - 1]);
-  // console.log(word[word.length - 1] === props.displayWord[word.length - 1]);
-
-  // useEffect(() => {
-  //   if (word[word.length - 1] === props.displayWord[word.length - 1]) {
-  //     setIsTrue("true");
-  //   } else {
-  //     setIsTrue("false");
-  //   }
-  // }, [word, props.displayWord]);
-  // console.log(props.displayWord.slice(0, word.length) === word);
 
   useEffect(() => {
     if (displayWord.slice(0, word.length) === word) {
@@ -99,38 +87,13 @@ const Input = ({
     backgroundColor: inputBackgroundColor,
   };
 
-  // const keys = "asdfghjkl";
-  // const wordLengths = [2, 3, 4];
-  // const combinations = [];
-
-  // for (let i = 0; i < wordLengths.length; i++) {
-  //   const wordLength = wordLengths[i];
-
-  //   for (let j = 0; j < keys.length; j++) {
-  //     const startChar = keys[j];
-
-  //     generateCombinations(startChar, wordLength, "");
-  //   }
-  // }
-
-  // function generateCombinations(currentChar, wordLength, currentWord) {
-  //   currentWord += currentChar;
-
-  //   if (currentWord.length === wordLength) {
-  //     combinations.push(currentWord);
-  //     return;
-  //   }
-
-  //   const currentIndex = keys.indexOf(currentChar);
-
-  //   for (let k = currentIndex + 1; k < keys.length; k++) {
-  //     const nextChar = keys[k];
-
-  //     generateCombinations(nextChar, wordLength, currentWord);
-  //   }
-  // }
-
-  // console.log(combinations);
+  // let accuracy = (1 - errorCount / totalWordCount) * 100;
+  // console.log("ERROR COUNT", errorCount);
+  useEffect(() => {
+    const newAccuracy =
+      totalWordCount > 0 ? (1 - errorCount / totalWordCount) * 100 : 100;
+    setAccuracy(newAccuracy);
+  }, [errorCount, totalWordCount]);
 
   return (
     <>
@@ -147,6 +110,11 @@ const Input = ({
         disabled={isCompleted}
         value={word}
       />
+      <div className="accuracy">
+        <p>Error Count: {errorCount}</p>
+        <p>Accuracy: {accuracy.toFixed(2)}%</p>
+        <p>Total Characters Typed: {totalWordCount}</p>
+      </div>
     </>
   );
 };
